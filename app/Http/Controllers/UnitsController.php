93d0d6c109bbf7eb5daf2d4b27b313755129aa56
Darshan Rathod <darshanrathod.info@gmail.com>
2019-04-11 22:55:27 +0530

    create crud api generator

---
+++ app/Http/Controllers/UnitsController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\UnitsRequest;
+use App\Units;
+
+class UnitsController extends Controller
+{
+    public function index()
+    {
+        $units = Units::latest()->get();
+
+        return response()->json($units);
+    }
+
+    public function store(UnitsRequest $request)
+    {
+        $units = Units::create($request->all());
+
+        return response()->json($units, 201);
+    }
+
+    public function show($id)
+    {
+        $units = Units::findOrFail($id);
+
+        return response()->json($units);
+    }
+
+    public function update(UnitsRequest $request, $id)
+    {
+        $units = Units::findOrFail($id);
+        $units->update($request->all());
+
+        return response()->json($units, 200);
+    }
+
+    public function destroy($id)
+    {
+        Units::destroy($id);
+
+        return response()->json(null, 204);
+    }
+}
